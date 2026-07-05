85625c4a3fcb95e2cd3e8c17610644e0ec725c7a
YanaSh <Jana20012010@mail.ru>
2026-07-05 17:05:26 +0700

    Last commit

---
+++ Kod/Kod.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct point {
+	double m_x;
+	double m_y;
+	point(double x, double y) {
+		m_x = x;
+		m_y = y; //присваиваем полю m_y
+	}
+};//поставили ;
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
+
+int main()//main c маленькой м
+{
+	int i;
+	for (i = 0; i < 5; i++) {// убрана лишняя ;
+		point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+	return 0;
+}
+//убран повтор 
